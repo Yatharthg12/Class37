d1d6b3a1f50e8ea794ea925253b0148c4bfd13e0
Yatharthg12 <72456920+Yatharthg12@users.noreply.github.com>
2020-12-26 15:44:54 +0530

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,46 @@
+class Game {
+  constructor(){}
+  
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+   
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  start(){
+    if(gameState === 0){
+      player = new Player();
+      player.getCount();
+      form = new Form()
+      form.display();
+    }
+  }
+  play(){
+    form.hidden();
+    textSize(30);
+    text("Game Start",120,100);
+    Player.getPlayerInfo();
+    if(allPlayers !== undefined){
+      var display_position = 130;
+      for(var i in allPlayers){
+        display_position = display_position+30;
+        textSize(15);
+        text(allPlayers[i].name +" "+allPlayers[i].distance,120,display_position)
+
+      }
+    }
+    if(keyDown(UP_ARROW)&& player.index !== null){
+      player.distance+= 50;
+      player.update();
+      
+    }
+  }
+}
